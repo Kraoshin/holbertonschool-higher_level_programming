c7d617ac0c3143d36fa93c2541c8fa6d58c16156
kraoshin <killian.kerleau@outlook.fr>
2025-02-10 12:08:23 +0100

    class Student that defines a student by: (based on 9-student.py)

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module for Student class."""
+
+
+class Student:
+    """Class Student."""
+    def __init__(self, first_name, last_name, age):
+        """Constructor for Student class."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Method to retrieve dict representation of Student instance."""
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    new_dict[key] = self.__dict__[key]
+            return new_dict
