63273f8fb4febfb4eba37f5db2b4bfe475a38f29
kraoshin <killian.kerleau@outlook.fr>
2025-02-19 09:45:10 +0100

    change the way to use url

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,13 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import requests
 import csv
 
 
 def fetch_and_print_posts():
     """
-    Fetches all posts from JSONPlaceholder and prints their titles.
+    Fetches posts from JSONPlaceholder and prints the status code
+    along with the title of each post if the request is successful.
     """
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
     print(f"Status Code: {response.status_code}")
 
     if response.status_code == 200:
@@ -18,28 +20,31 @@ def fetch_and_print_posts():
 
 def fetch_and_save_posts():
     """
-    Fetches all posts from JSONPlaceholder and saves them to a CSV file.
+    Fetches posts from JSONPlaceholder and saves them to a CSV file
+    named 'posts.csv' if the request is successful. The CSV file
+    includes columns: id, title, and body.
     """
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
 
     if response.status_code == 200:
         posts = response.json()
-        data = [
+        struc_data = [
             {
-                'id': post['id'],
-                'title': post['title'],
-                'body': post['body']
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
             }
             for post in posts
         ]
 
-        with open('posts.csv', 'w', newline='', encoding="utf-8") as csvfile:
-            fieldnames = ['id', 'title', 'body']
+        with open("posts.csv", "w", newline='', encoding="utf-8") as csvfile:
+            fieldnames = ["id", "title", "body"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
             writer.writeheader()
-            writer.writerows(data)
+            writer.writerows(struc_data)
 
-# Execute functions
-    fetch_and_print_posts()
-    fetch_and_save_posts()
+
+# Execute the functions
+fetch_and_print_posts()
+fetch_and_save_posts()
