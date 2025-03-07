bea831e1cd4b659717dbc926e3de43d07517829b
kraoshin <killian.kerleau@outlook.fr>
2025-03-07 10:37:49 +0100

    add pycodestyle

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""script that changes the nameof a State object from the database
+"""script that changes the name of a State object from the database
 hbtn_0e_6_usa"""
 
 import sys
@@ -19,7 +19,7 @@ if __name__ == "__main__":
                            .format(username, password, database))
 
     Session = sessionmaker(bind=engine)
-    
+
     session = Session()
 
     state = session.query(State).filter(State.id == state_id).first()
