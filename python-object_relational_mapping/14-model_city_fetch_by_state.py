6f46a999b1208f3d276ed218d18e382a68c1c0bc
kraoshin <killian.kerleau@outlook.fr>
2025-03-07 11:12:20 +0100

    print all city object

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
-"""
-Prints all City objects from the database hbtn_0e_14_usa.
-"""
-
+"""print all city object"""
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1],
+                                   sys.argv[2],
+                                   sys.argv[3]))
 
-    engine = create_engine(
-        f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/'
-        f'{database_name}'
-        )
+    Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker(engine)
     session = Session()
+
     cities = (
         session.query(City, State)
         .filter(City.state_id == State.id)
-        .order_by(City.id)
-        .all()
-        )
+        .order_by(City.id).all()
+    )
 
-    for city, state in cities:
-        print(f"{state.name}: ({city.id}) {city.name}")
+    for cities, state in cities:
+        print("{}: ({}) {}".format(state.name, cities.id, cities.name))
     session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -7,6 +7,7 @@ Defines the City class, which represents a city in the database.
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
+Base = declarative_base()
 
 
 class City(Base):
