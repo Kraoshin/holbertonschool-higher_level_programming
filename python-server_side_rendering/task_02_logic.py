0d80cb98fc287221391cbf5d5556b836de935d5f
kraoshin <killian.kerleau@outlook.fr>
2025-03-21 12:50:10 +0100

    modified thanks to pablo

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from flask import Flask, render_template
 import json
 
@@ -15,14 +16,12 @@ def about():
 def contact():
     return render_template('contact.html')
 
-@app.route("/items")
+@app.route('/items')
 def items():
     with open('items.json', 'r') as file:
-        data = json.load(file)
-    items = data.get('items', [])
-    return render_template('items.html', items=items)
-
-    
+        data  = json.load(file)
+    list = data.get('items', [])
+    return render_template('items.html', list=list)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -1,43 +1,61 @@
-from flask import Flask, request, render_template
+#!/usr/bin/python3
+from flask import Flask, render_template, request
 import json
 import csv
 
 app = Flask(__name__)
 
-def read_json_file(file_path):
-    with open(file_path, 'r') as file:
+def json_file(filepath):
+    with open(filepath, 'r') as file:
         return json.load(file)
-
-def read_csv_file(file_path):
+    
+def csv_file(filepath):
     products = []
-    with open(file_path, 'r') as file:
+    with open(filepath, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
             row['id'] = int(row['id'])
             row['price'] = float(row['price'])
             products.append(row)
-    return products
+        return products
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    with open('items.json', 'r') as file:
+        data  = json.load(file)
+    list = data.get('items', [])
+    return render_template('items.html', list=list)
 
 @app.route('/products')
-def display_products():
+def products():
     source = request.args.get('source')
-    product_id = request.args.get('id')
-    products = []
-
+    id = request.args.get('id', type=int)
+    
     if source == 'json':
-        products = read_json_file('products.json')
+        products = json_file('products.json')
     elif source == 'csv':
-        products = read_csv_file('products.csv')
+        products = csv_file('products.csv')
     else:
-        return render_template('product_display.html', error="Wrong source")
-
-    if product_id:
-        product_id = int(product_id)
-        products = [product for product in products if product['id'] == product_id]
+        return render_template('product_display.html', message="Wrong source")
+    
+    if id:
+        products = [product for product in products if product['id'] == id]
         if not products:
-            return render_template('product_display.html', error="Product not found")
-
+            return render_template('product_display.html', message="Product not found")
+    
     return render_template('product_display.html', products=products)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
